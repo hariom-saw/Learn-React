196c686556cc9b5f66f4d7ab7dd3e0436906e0b7
hariom-saw <hariomsaw@gmail.com>
2024-02-23 16:11:59 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,27 +1,5 @@
-
-// const heading = document.createElement('h1');
-// heading.innerHTML = "Hello Javascript";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-
-// const heading = React.createElement("h1", { id: "heading", datacustom: "test"}, "Hello world by React");
-// const root = ReactDOM.createRoot(document.getElementById('root')).render(heading);
-
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
@@ -29,8 +7,8 @@ const parent = React.createElement("div", { id: "parent" }, [
         React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
     React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
+        React.createElement("h1", {}, "I'm an h1 tag3"),
+        React.createElement("h2", {}, "I'm an h2 tag3"),
     ]),
 ]);
 
