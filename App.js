31b7c964186a516f56b67b960a6a49c59dfd6404
hariom-saw <hariomsaw@gmail.com>
2024-02-23 19:56:28 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag3"),
-        React.createElement("h2", {}, "I'm an h2 tag3"),
-    ]),
-]);
+// JS (transpiled before it reaches the JS) - parcel - Babel.
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render).
 
-//JSX
 
-console.log(parent); // object
+// React Element
+const heading = (
+    <div>
+        <h1 id="heading-element"> Hello React, Its React Element </h1>
+    </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+/**
+ * React Components
+ * - Class based Components  - Old
+ * - Functional based Components - New  => It just JS function.
+ *      Functional Component is a JS function which return React Element(JSX).
+ *      Functional Component is a normal JS function which return some JSX.
+ * JSX is React Elment.
+ */
 
-root.render(parent);
+
+const Title = () =>  <h2 className="head">This is heading 2</h2>;
+
+// Using Nested Component,It all so called "Component Composition in React".
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        {heading}
+        <h1 className="heading">Hello Hariom, its React Functional Component</h1>
+    </div>
+);
+const HeadingComponent2 = () => <h1 className="fun-heading">Hello Hariom, its React Functional Component</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent />);
